ff1109e6783cdbb219e1cb273d241f25258cd8ce
Peniel-codes9 <pipikwarteng@gmail.com>
2026-09-20 13:41:09 -1200

    Delete week_4/library_api directory

--- week_4/library_api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const bookRoutes = require("./book.routes");
-
-const router = express.Router();
-
-router.use("/books", bookRoutes);
-
-module.exports = router;
